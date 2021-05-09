c66b483b06f2dd12134dc9a93da67bb44bda7735
Mihoc Roxana <roxana.mihoc67@gmail.com>
2021-05-10 00:25:41 +0300

    detalii spital in home

--- project/src/components/Home/Home.js
+++ project/src/components/Home/Home.js
@@ -6,13 +6,34 @@ const Home = () => {
     <React.Fragment>
       <Navbar />
 
-      <div className="container">
-        <h1 className="text-center" style={{ paddingTop: "30%" }}>
-          Home
+      <div classNme="container">
+        <h1 className="text-left" >
+          Bun venit, Alex!
         </h1>
+        <p className="text-left" style={{ fontSize: "20px" }}>
+          Spital: Spitalul Universitar de Urgență București
+        </p>
+        <p className="text-left" style={{ fontSize: "20px" }}>
+          Adresa spital: Splaiul Independenței 169, București 050098
+        </p>
+        <p className="text-left" style={{ fontSize: "20px" }}>
+          Administrator spital: Paladi Alexandru
+        </p>
+        <p className="text-left" style={{ fontSize: "20px"}}> 
+          Șoferi:
+          <ul style={{ marginLeft: "25px"}}>
+            <li>John Cena</li>
+            <li>Brendan Ellis</li>
+            <li>Anika Bailey</li>
+          </ul>
+        </p>
+
+
       </div>
     </React.Fragment>
   );
 };
 
 export default Home;
+
+
