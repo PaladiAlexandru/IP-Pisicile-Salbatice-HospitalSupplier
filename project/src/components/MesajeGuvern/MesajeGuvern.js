c4b3a709c066c39012e0d87b01fde67fef2de696
Clara Sima <79227862+clarasima@users.noreply.github.com>
2021-05-17 04:56:04 +0300

    modificari la mesaje

--- project/src/components/MesajeGuvern/MesajeGuvern.js
+++ project/src/components/MesajeGuvern/MesajeGuvern.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import { send } from "emailjs-com";
 
+
 const MesajeGuvern = () => {
   const [toSend, setToSend] = useState({
     message: '',
@@ -19,33 +20,46 @@ const MesajeGuvern = () => {
       console.log('SUCCES', response.status, response.text);
     }).catch((err) => {
       console.log('Eroare', err);
+  
     });
+    
   };
 
   const handleChange = (e) => {
     setToSend({ ...toSend, [e.target.name]: e.target.value });
   };
 
-  return (
+
+
+return (
     <React.Fragment>
       <Navbar />
       <div className="container">
+      <h1>Mesaje Guvern</h1>
         <form onSubmit={onSubmit}>
-          <input type='text'
-            name='subject'
-            className="form-control"
-            placeholder='Subiect'
-            value={toSend.subject}
-            onChange={handleChange}
-          />
-          <input type='text'
-            name='message'
-            className="form-control"
-            placeholder='Mesaj'
-            value={toSend.message}
-            onChange={handleChange}
-          />
-          <button type='submit' className="btn btn-primary">Submit</button>
+          <div className="form-group">
+            <label >Subiect</label>
+            <input  type='text' 
+                    name='subject'
+                    className="form-control" 
+                    placeholder='Subiect'
+                    value={toSend.subject}
+                    onChange={handleChange}
+                    />
+          </div>
+          <div className="form-group">
+            <label >Mesaj</label>
+            <input  type='text' 
+                    name='message'
+                    className="form-control" 
+                    placeholder='Scrie aici un mesaj...'
+                    value={toSend.message}
+                    onChange={handleChange}
+                    />
+          </div>
+          { <button type='submit' 
+          className="btn btn-primary"> Submit</button> }
+        
         </form>
       </div>
     </React.Fragment>
